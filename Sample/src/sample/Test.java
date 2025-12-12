534c0ab0040904372a8c0f7c053f40273a4fc612
tomohiro.shimpo <tomohiro.shimpo@gmail.com>
2025-12-12 16:19:04 +0900

    Testクラスを作成

---
+++ Sample/src/sample/Test.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Kiramex");
+        System.out.println("Java");
+        System.out.println("学習を始めましょう!");
+        System.out.println("Gitの練習です");
+    }
+}
